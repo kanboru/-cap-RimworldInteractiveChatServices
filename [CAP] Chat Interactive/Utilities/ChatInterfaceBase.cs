1940b816cc62fe4dbb00b652c6455a75f56c770d
ekudram <ekudram@hotmail.com>
2025-11-28 14:28:33 -0700

    "Corrected license headers to properly reflect AGPLv3 licensing. All code in this repository is licensed under GNU Affero General Public License v3.0."

--- [CAP] Chat Interactive/Utilities/ChatInterfaceBase.cs
+++ [CAP] Chat Interactive/Utilities/ChatInterfaceBase.cs
@@ -1,8 +1,20 @@
 ﻿// ChatInterfaceBase.cs
-// Copyright (c) Captolamia. All rights reserved.
-// Licensed under the AGPLv3 License. See LICENSE file in the project root for full license information.
+// Copyright (c) Captolamia
+// This file is part of CAP Chat Interactive.
+// 
+// CAP Chat Interactive is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// CAP Chat Interactive is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with CAP Chat Interactive. If not, see <https://www.gnu.org/licenses/>.
 // Abstract base class for chat interfaces to handle message parsing and system events
-using System;
 using Verse;
 
 namespace CAP_ChatInteractive
--- [CAP] Chat Interactive/Windows/Dialogs/Dialog_DebugRaces.cs
+++ [CAP] Chat Interactive/Windows/Dialogs/Dialog_DebugRaces.cs
@@ -1,4 +1,18 @@
-﻿// Update Dialog_DebugRaces.cs to show the comparison more clearly
+﻿// Copyright (c) Captolamia
+// This file is part of CAP Chat Interactive.
+// 
+// CAP Chat Interactive is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// CAP Chat Interactive is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with CAP Chat Interactive. If not, see <https://www.gnu.org/licenses/>.
 using CAP_ChatInteractive;
 using _CAP__Chat_Interactive.Utilities;
 using System.Collections.Generic;
