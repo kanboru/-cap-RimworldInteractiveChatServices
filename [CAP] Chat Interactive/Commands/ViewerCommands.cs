85c0f3bcf379424001700338ff5cab71329ef03b
kanboru <kanboru201@outlook.es>
2026-01-16 19:11:06 +0100

    colonists now counts viewer pawns instead of viewers(oops)

--- [CAP] Chat Interactive/Commands/ViewerCommands.cs
+++ [CAP] Chat Interactive/Commands/ViewerCommands.cs
@@ -379,7 +379,21 @@ namespace CAP_ChatInteractive.Commands.ViewerCommands
 
             return $"There are {colonistCount}({viewerCount} viewers) and {animalCount} colony animals.";
         }
+    }
 
+    public class Stockpile : ChatCommand
+    {
+        public override string Name => "stockpile";
+        public override string Execute(ChatMessageWrapper messageWrapper, string[] args)
+        {
+            var assignmentManager = CAPChatInteractiveMod.GetPawnAssignmentManager();
+            //var colonistList = Current.Game.PlayerHomeMaps.SelectMany(m => m.mapPawns.FreeColonistsSpawned);
+            //var animalsList = Current.Game.PlayerHomeMaps.SelectMany(m => m.mapPawns.ColonyAnimals);
+            int colonistCount = Current.Game.PlayerHomeMaps.Sum(m => m.mapPawns.FreeColonistsSpawnedCount);
+            int animalCount = Current.Game.PlayerHomeMaps.Sum(m => m.mapPawns.ColonyAnimals.Count);
+            int viewerCount = assignmentManager.viewerPawnAssignments.Count;
 
+            return $"There are {colonistCount} colonists({viewerCount} viewers) and {animalCount} colony animals.";
+        }
     }
 }
